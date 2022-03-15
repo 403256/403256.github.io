d7ef908edc7f0bb9fd1d2bb67ebb5ffb837aacae
403256 <97700409+403256@users.noreply.github.com>
2022-03-15 12:22:54 -0700

    Updates from 3/15 -- adding sprites & starting event handlers

--- engine/main.js
+++ engine/main.js
@@ -19,6 +19,17 @@ class GameEngine {
     this.update = updateFctn;
 
     this.sprites = [];
+
+    this.activeCamera = new Camera(0, 0, 1);
+    this.cameras = [this.activeCamera];
+
+    // Add event handlers
+    document.addEventListener('keydown', this);
+    document.addEventListener('keyup', this);
+  }
+
+  handleEvent(event) {
+    console.log(event.type);
   }
 
   start() {
@@ -42,25 +53,39 @@ class GameEngine {
   draw() {
     let length = this.sprites.length;
     for(let i = 0; i < length; i++) {
-      let sprite = this.sprites[i]
-      this.ctx.fillStyle = sprite.imgSrc;
-      this.ctx.fillRect(sprite.x, sprite.y, sprite.width, sprite.height);
+      let sprite = this.sprites[i];
+      this.activeCamera.draw(this.ctx,
+          sprite.src, sprite.sx, sprite.sy, sprite.sWidth, sprite.sHeight,
+          sprite.x, sprite.y, sprite.width, sprite.height);
     }
+  }
+
+  changeCamera(cameraNumber) {
+    this.activeCamera = this.cameras[cameraNumber];
+  }
+
+  zoomCamera(zoomFactor) {
+    this.activeCamera.changeZoom(zoomFactor);
   }
 }
 
 class Sprite {
   constructor(x, y, width, height,
       imgSrc,
-      {scale = 1, visible = true, depth = 0, dx = 0, dy = 0}) {
+      {scale = 1, visible = true, depth = 0, sx = 0, sy = 0, sWidth = width, sHeight = height, dx = 0, dy = 0}) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+    this.sx = sx;
+    this.sy = sy;
+    this.sWidth = sWidth;
+    this.sHeight = sHeight;
     this.scale = scale;
     this.visible = visible;
 
-    this.imgSrc = imgSrc;
+    this.src = new Image();
+    this.src.src = imgSrc;
     this.depth = depth;
 
     this.dx = dx;
@@ -74,21 +99,37 @@ class Sprite {
 }
 
 class Camera {
+  /**
+   * Creates a camera object to control which part of the screen is displayed
+   * @param {Number} x         Starting left edge of camera
+   * @param {Number} y         Starting right edge of camera
+   * @param {Number} [zoom=1]  Starting zoom
+   */
   constructor(x, y, zoom = 1) {
     this.x = x;
     this.y = y;
     this.zoom = zoom;
   }
 
+  draw(ctx, src, sx, sy, sWidth, sHeight, x, y, width, height) {
+    x = (x - this.x);
+    y = (y - this.y);
+    width = (width * this.zoom);
+    height = (height * this.zoom);
+
+    ctx.drawImage(src,
+        sx, sy, sWidth, sHeight,
+        x, y, width, height);
+  }
+
+  changeZoom(zoomFactor = 'reset') {
+    if(typeof zoomFactor == 'number')
+      this.zoom *= zoomFactor;
+    else if(zoomFactor == 'reset')
+      this.zoom = 1;
+  }
+
   move() {
-
-  }
-
-  off() {
-
-  }
-
-  on() {
 
   }
 }
--- testing.js
+++ testing.js
@@ -1,9 +1,10 @@
 let game = new GameEngine(loopMe);
-let player = new Sprite(0, 100, 10, 30, '#98ff98', {dx: 10});
+let player = new Sprite(0, 100, 20, 32, 'player1.png', {dx: 10});
 game.addSprite(player);
 
 function loopMe() {
   player.move();
+  game.zoomCamera(2);
   if(player.x > 100) game.stop();
 }
 
