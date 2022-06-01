ac93996dcf64b2dcdf6bca4a4d5fb22bce3c6356
Nate <403256@bsd48.org>
2022-05-31 21:32:15 -0700

    Fix 'this' scope errors

--- engine/main.js
+++ engine/main.js
@@ -1,31 +1,31 @@
 const game = {
   initial: (update, canvasParent = document.body) => {
     // Create canvas element for game
-    this.canvas = document.createElement('CANVAS');
-    this.canvas.width = 1000;
-    this.canvas.height = 750;
-    this.canvas.style.background = '#222';
-    this.canvas.style.position = 'absolute';
-    this.canvas.style.left = '50%';
-    this.canvas.style.top = '50%';
-    this.canvas.style.transform = 'translate(-50%, -50%)';
-    canvasParent.appendChild(this.canvas);
+    game.canvas = document.createElement('CANVAS');
+    game.canvas.width = 1000;
+    game.canvas.height = 750;
+    game.canvas.style.background = '#222';
+    game.canvas.style.position = 'absolute';
+    game.canvas.style.left = '50%';
+    game.canvas.style.top = '50%';
+    game.canvas.style.transform = 'translate(-50%, -50%)';
+    canvasParent.appendChild(game.canvas);
 
     // Get canvas context for drawing on canvas
-    this.ctx = this.canvas.getContext('2d');
+    game.ctx = game.canvas.getContext('2d');
 
-    this.update = updateFctn;
+    game.update = update;
 
-    this.sprites = [];
-    this.maps = [];
+    game.sprites = [];
+    game.maps = [];
 
-    this.cameras = [];
-    this.addCamera(0, 0, 1);
-    this.activeCamera = this.cameras[0];
+    game.cameras = [];
+    game.addCamera(0, 0, 1);
+    game.activeCamera = game.cameras[0];
 
     // Add event handlers
-    document.addEventListener('keydown', this);
-    document.addEventListener('keyup', this);
+    document.addEventListener('keydown', game);
+    document.addEventListener('keyup', game);
   },
 
   handleEvent : (event) => {
@@ -34,44 +34,44 @@ const game = {
 
   start : (forceStart = false) => {
     if(!forceStart && document.readyState != 'complete') {
-      window.addEventListener('load', this.start);
+      window.addEventListener('load', game.start);
       return;
     }
-    this.animation = requestAnimationFrame(this.loop);
+    game.animation = requestAnimationFrame(game.loop);
   },
 
   stop : () => {
-    cancelAnimationFrame(this.animation);
+    cancelAnimationFrame(game.animation);
   },
 
   loop : () => {
-    this.animation = requestAnimationFrame(this.loop);
-    this.activeCamera.drawScreen(this.currentMap);
-    this.update();
+    game.animation = requestAnimationFrame(game.loop);
+    game.activeCamera.drawScreen(game.currentMap);
+    game.update();
   },
 
   addCamera : (x, y, zoom) => {
-    let camera = new Camera(this.canvas.width, this.canvas.height, x, y, zoom);
-    return this.cameras.push(camera) - 1;
+    let camera = new Camera(game.canvas.width, game.canvas.height, x, y, zoom);
+    return game.cameras.push(camera) - 1;
   },
 
   changeCamera : (cameraID) => {
-    this.activeCamera = this.cameras[cameraID];
+    game.activeCamera = game.cameras[cameraID];
   },
 
   addMap : (map) => {
-    let index = this.maps.push(map) - 1;
+    let index = game.maps.push(map) - 1;
 
-    if(index == 0) this.currentMap = map;
+    if(index == 0) game.currentMap = map;
 
     return index;
   },
 
   changeMap : (index) => {
-    this.currentMap = this.maps[index];
+    game.currentMap = game.maps[index];
   },
 
   zoomCamera : (zoomFactor) => {
-    this.activeCamera.changeZoom(zoomFactor);
+    game.activeCamera.changeZoom(zoomFactor);
   },
 }
